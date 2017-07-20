c75d81d2d3929a876c6a98c9ca7ab40152048b62
Avais Khatri <avaiskhatri@hotmail.com>
2017-07-20 21:59:58 +0500

    Update 4

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -12,9 +12,29 @@ function ($scope, $stateParams) {
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
 function ($scope, $stateParams) {
-    $scope.labels = ["Regirator", "AC", "Others"];
-    $scope.data = [300, 500, 100];
+    $scope.labels = ["Refigrator", "AC", "Others"];
+    var alldata = $scope.data = [350, 450, 180];
+    var sum = alldata.reduce(function(a, b) { return a + b; }, 0);
+    
+    Chart.pluginService.register({
+      beforeDraw: function(chart) {
+        var width = chart.chart.width,
+            height = chart.chart.height,
+            ctx = chart.chart.ctx;
 
+        ctx.restore();
+        var fontSize = (height / 140).toFixed(2);
+        ctx.font = fontSize + "em sans-serif";
+        ctx.textBaseline = "middle";
+
+        var text = sum+" Un",
+            textX = Math.round((width - ctx.measureText(text).width) / 2),
+            textY = height / 2;
+
+        ctx.fillText(text, textX, textY);
+        ctx.save();
+      }
+    });
 }])
    
 .controller('devicesCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
